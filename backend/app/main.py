a31d48d5890bb6cdadc4ee66f5f66175374cf620
comnk <vhsiao36@gmail.com>
2026-04-03 19:22:59 -0700

    hopeful fix, small CORS error

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://dcc-eosin.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://dcc-eosin.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
